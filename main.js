e0fc9f665a930832bf207b8ebac7a82fbfe35c7d
SanjithBC <111051325+SanjithBC@users.noreply.github.com>
2023-06-23 19:59:20 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 status = "";
+objects = [];
 
 function setup() {
+    video = createCapture(VIDEO);
+    video.hide();
     canvas = createCanvas(480, 380);
     canvas.center();
     canvas.hide();
@@ -8,6 +11,28 @@ function setup() {
 
 function draw() {
     image(canvas, 0, 0, 480, 380);
+        if(status !="") {
+            objectDetector.detect(video, gotResults)
+            for (i = 0; i < objects.length; i++) {
+                fill("#ff0000");
+                percent = floor(objects[i].confidence * 100);
+                text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+                noFill();
+                stroke("#ff0000");
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            }
+        }
+        if(objects[i].label == object_name) {
+            video.stop();
+            objectDetector.detect(gotResults);
+            document.getElementById("status").innerHTML = object_name + "Found!";
+            synth = window.speechSynthesis;
+            SpeechSynthesisUtterance(object_name + "Found!");
+            synth.speak(utterThis);
+        }
+        else {
+            document.getElementById("status").innerHTML = object_name + "not found";
+        }
 }
 
 function start() {
@@ -18,4 +43,11 @@ function start() {
 function modelLoaded() {
     console.log("Model Loaded!");
     status = true;
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
 }
